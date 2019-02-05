f78a7ca85d3ed8b15d28af85db76b21053d848f0
Tu Vo <tuvo1106@gmail.com>
2019-02-05 23:00:23 +0000

    Add test2

--- 0x02-shell_redirections/git-deploy.sh
+++ 0x02-shell_redirections/git-deploy.sh
@@ -3,4 +3,4 @@ read -r -p 'Commit message: ' msg # prompt user for commit message
 git add *                         # track all files
 git add -u                        # track deletes
 git commit -m "$msg"              # commit with message
-git push 			  # push
+git push             			  # push
