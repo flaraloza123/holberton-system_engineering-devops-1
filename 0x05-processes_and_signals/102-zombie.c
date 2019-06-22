ae267a76f32b3394a05b9d34b720614b9488b397
Tu Vo <tuvo1106@gmail.com>
2019-06-22 01:52:41 +0000

    Add source code for 102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,1 +1,39 @@
-#!/usr/bin/env bash
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+/**
+ * infinite_while - infinite loop
+ * Return: 1 on error, 0 on success
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - entry point
+ * Return: 1 on error, 0 on success
+ */
+int main(void)
+{
+	int i = 5;
+
+	while (i--)
+	{
+		pid_t child_pid = fork();
+
+		if (child_pid > 0)
+			printf("Zombie process created, PID: %i\n", child_pid);
+		else
+			exit(0);
+	}
+	infinite_while();
+
+	return (EXIT_SUCCESS);
+}
