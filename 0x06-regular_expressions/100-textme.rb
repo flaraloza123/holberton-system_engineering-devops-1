10dc3af1dc94b3d4c7c4e8e8a82d0cf296000baf
Tu Vo <tuvo1106@gmail.com>
2019-07-08 11:26:31 +0000

    Update 100 to be one line

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,3 @@
 #!/usr/bin/env ruby
 
-print ARGV[0].scan(/((?<=from:)\+?[\w]+)/).join
-print ","
-print ARGV[0].scan(/((?<=to:)\+?[\w]+)/).join
-print ","
-puts ARGV[0].scan(/((?<=flags:)[-\w:]+)/).join
+puts ARGV[0].scan(/((?<=from:)\+?[\w]+|(?<=to:)\+?[\w]+|(?<=flags:)[-\w:]+)/).join(',')
