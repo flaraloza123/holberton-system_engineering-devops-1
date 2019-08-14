7a6318f956742026702d2eb4cd6a219eecd4bbc0
Tu Vo <tuvo1106@gmail.com>
2019-08-14 17:09:03 +0000

    Upload task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,1 +1,19 @@
-#!/usr/bin/env bash
+# create a custom HTTP header response
+
+package { 'nginx':
+  ensure => installed,
+  name   => 'nginx',
+}
+
+file_line { 'header':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'add_header X-Served-By "$HOSTNAME";',
+  multiple => true
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
