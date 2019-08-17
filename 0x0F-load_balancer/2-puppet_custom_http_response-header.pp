45185462c6addf5743cdd8eaea33be9dc6a7dd67
Tu Vo <tuvo1106@gmail.com>
2019-08-17 16:47:16 +0000

    Refactor advanced tasj

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -4,17 +4,20 @@ exec { 'update':
   command => '/usr/bin/apt-get update',
 }
 
-
 package { 'nginx':
-  ensure   => installed,
-  require  => Exec['update'],
+  ensure  => installed,
+  require => Exec['update'],
 }
 
 file_line { 'header':
-  ensure   => present,
-  path     => '/etc/nginx/sites-available/default',
-  after    => 'server_name _;',
-  line     => 'add_header X-Served-By "$HOSTNAME";',
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'add_header X-Served-By "$HOSTNAME";',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
 }
 
 service { 'nginx':
