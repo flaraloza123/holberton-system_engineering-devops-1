8318fdaff624b8f4610738db36391d3867bb74c6
Tu Vo <tuvo1106@gmail.com>
2019-09-16 22:41:19 +0000

    Refactor task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,10 +11,9 @@ from sys import argv
 
 def get_employee_tasks(user_id):
     '''Retrieves employees and tasks'''
-    root_url = 'https://jsonplaceholder.typicode.com/'
-    user_req = root_url + 'users/{}'.format(user_id)
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_req = url + 'users/{}'.format(user_id)
     employee = requests.get(user_req).json()
-    employee_id = employee.get('id')
     task_req = root_url + 'todos?userId={}'.format(employee_id)
     tasks = requests.get(task_req).json()
     return {"employee": employee, "tasks": tasks}
@@ -22,18 +21,16 @@ def get_employee_tasks(user_id):
 
 def export_to_csv(data):
     with open('{}.csv'.format(argv[1]), mode='w') as _file:
-        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                      "TASK_TITLE"]
-        writer = csv.DictWriter(_file, fieldnames=fieldnames)
+        writer = csv.DictWriter(_file, quoting=csv.QUOTE_ALL)
         e_id = data.get('employee').get('id')
         e_username = data.get('employee').get('username')
         for d in data.get('tasks'):
-            writer.writerow({
-                "USER_ID": e_id,
-                "USERNAME": e_username,
-                "TASK_COMPLETED_STATUS": d.get('completed'),
-                "TASK_TITLE": d.get('title')
-                })
+            writer.writerow([
+                e_id,
+                e_username,
+                d.get('completed'),
+                d.get('title')
+                ])
 
 
 if __name__ == '__main__':
