ba442b970ee1134b01bde5805fa06a912bf95fe0
Tu Vo <tuvo1106@gmail.com>
2020-06-29 18:33:18 -0700

    Update 100-count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -18,16 +18,19 @@ def count_words(subreddit, word_list, res=defaultdict(int), after=None):
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     if after:
         url += '?after={}'.format(after)
-    r = requests.get(url, headers=headers, allow_redirects=False).json()
-    titles = r.get('data').get('children')
-    for t in titles:
-        c = Counter(t.get('data').get('title').lower().split(' '))
-        for word in word_list:
-            res[word] += c.get(word.lower(), 0)
-    after = r.get('data').get('after')
-    if after:
-        return count_words(subreddit, word_list, res, after)
-    for k, v in sorted(res.items(),
-                       key=lambda x: (x[1], x[0]),
-                       reverse=True):
-        print('{}: {}'.format(k, v))
+    try:
+        r = requests.get(url, headers=headers, allow_redirects=False).json()
+        titles = r.get('data').get('children')
+        for t in titles:
+            c = Counter(t.get('data').get('title').lower().split(' '))
+            for word in word_list:
+                if word in c:
+                    res[word] += c.get(word.lower())
+        after = r.get('data').get('after')
+        if after:
+            return count_words(subreddit, word_list, res, after)
+        first_sort = sorted(res.items(), key=lambda x: x[0])
+        for k, v in sorted(first_sort, key=lambda x: x[1], reverse=True):
+            print('{}: {}'.format(k, v))
+    except:
+        return
