9f4a4833e35f1c091ba6e8a1b3174a9cc80d1db7
Tu Vo <tuvo1106@gmail.com>
2020-06-29 18:34:28 -0700

    Update 100-count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -24,7 +24,7 @@ def count_words(subreddit, word_list, res=defaultdict(int), after=None):
         for t in titles:
             c = Counter(t.get('data').get('title').lower().split(' '))
             for word in word_list:
-                if word in c:
+                if word.lower() in c:
                     res[word] += c.get(word.lower())
         after = r.get('data').get('after')
         if after:
