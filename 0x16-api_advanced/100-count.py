2c74f7055792ac6f9b32b38b0e132d096c1b1c13
Tu Vo <tuvo1106@gmail.com>
2020-06-29 18:19:00 -0700

    Update 100-count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,6 +5,7 @@ count words
 '''
 
 from collections import Counter, defaultdict
+import re
 import requests
 
 
@@ -17,29 +18,16 @@ def count_words(subreddit, word_list, res=defaultdict(int), after=None):
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     if after:
         url += '?after={}'.format(after)
-    try:
-        r = requests.get(url, headers=headers, allow_redirects=False).json()
-        titles = r.get('data').get('children')
-        for t in titles:
-            split_title = t.get('data').get('title').lower().split(' ')
-            count(res, word_list, split_title)
-        after = r.get('data').get('after')
-        if not after:
-            return print_sorted_dict(res)
+    r = requests.get(url, headers=headers, allow_redirects=False).json()
+    titles = r.get('data').get('children')
+    for t in titles:
+        c = Counter(t.get('data').get('title').lower().split(' '))
+        for word in word_list:
+            res[word] += c.get(word.lower(), 0)
+    after = r.get('data').get('after')
+    if after:
         return count_words(subreddit, word_list, res, after)
-    except Exception:
-        return
-
-
-def count(res, word_list, title):
-    ''' count words in a sentence from a given list'''
-    word_list = [x for x in word_list]
-    for word in word_list:
-        if word.lower() in title:
-            res[word] += 1
-
-
-def print_sorted_dict(res):
-    ''' print sorted dictionary '''
-    for k, v in sorted(res.items(), key=lambda x: x[1], reverse=True):
+    for k, v in sorted(res.items(),
+                       key=lambda x: (x[1], x[0]),
+                       reverse=True):
         print('{}: {}'.format(k, v))
