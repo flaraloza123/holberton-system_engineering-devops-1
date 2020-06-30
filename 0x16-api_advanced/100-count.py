1c019af69d58124daffb2fd5003163f6447d30bb
Tu Vo <tuvo1106@gmail.com>
2020-06-29 17:30:07 -0700

    Update 100-count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
+'''
+count words
+'''
+
+from collections import Counter, defaultdict
 import requests
 
-'''
-count
-'''
 
-
-def count_words(subreddit, word_list, _dict={}, after=None):
+def count_words(subreddit, word_list, res=defaultdict(int), after=None):
     ''' count words in a subreddit '''
     agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6)\
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132i\
@@ -18,31 +19,27 @@ def count_words(subreddit, word_list, _dict={}, after=None):
         url += '?after={}'.format(after)
     try:
         r = requests.get(url, headers=headers, allow_redirects=False).json()
-        _list = r.get('data').get('children')
-        for li in _list:
-            title = li.get('data').get('title').lower().split(' ')
-            _dict = count(_dict, word_list, title)
+        titles = r.get('data').get('children')
+        for t in titles:
+            split_title = t.get('data').get('title').lower().split(' ')
+            count(res, word_list, split_title)
         after = r.get('data').get('after')
-        if after is None:
-            print_sorted_dict(_dict)
-            return
-        return count_words(subreddit, word_list, _dict, after)
+        if not after:
+            return print_sorted_dict(res)
+        return count_words(subreddit, word_list, res, after)
     except Exception:
-        return None
+        return
 
 
-def count(_dict, word_list, sentence):
+def count(res, word_list, title):
     ''' count words in a sentence from a given list'''
-    for s in sentence:
-        if s in set([x.lower() for x in word_list]):
-            if _dict.get(s):
-                _dict[s] += 1
-            else:
-                _dict[s] = 1
-    return _dict
+    word_list = [x.lower() for x in word_list]
+    for word in word_list:
+        if word in title:
+            res[word] += 1
 
 
-def print_sorted_dict(_dict):
+def print_sorted_dict(res):
     ''' print sorted dictionary '''
-    for k, v in sorted(_dict.items(), key=lambda x: x[1], reverse=True):
+    for k, v in sorted(res.items(), key=lambda x: x[1], reverse=True):
         print('{}: {}'.format(k, v))
